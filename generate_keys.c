42fb9794dac710082f764f78d3bf36c8d52bb400
Lucas Santos <lucasnascimentoss@gmail.com>
2019-03-29 10:59:37 -0300

    DONE: Geracao da chave privada binario OpenSSL TODO: Geracao da chave publica pelo bitcoin-core

--- generate_keys.c
+++ generate_keys.c
@@ -1,21 +1,29 @@
-/* Bibliotecas */
+/* OpenSSL */
 // Biblioteca de IO
 #include <openssl/bio.h>
 // Biblioteca de erros
 #include <openssl/err.h>
 // Biblioteca de curva eliptica
 #include <openssl/ec.h>
-// Biblioteca com o formato das chaves
+// Biblioteca para tratamendo de BIGNUM
+#include <openssl/bn.h>
+// Biblioteca para formatação de chaves
 #include <openssl/pem.h>
+
+/* secp256k1 */
+// Biblioteca de curva eliptica
+#include "../secp256k1/include/secp256k1.h"
 
 // Curva eliptica utilizada na criptografia de chave publica do Bitcoin
 #define DEFAULT_ELLIPTIC_CURVE "secp256k1"
 
 int main() {
              
+    secp256k1_context *ctx = NULL;
     BIO *io_print = NULL;
-    //BIO *io_file = NULL;
     EC_KEY *elliptic_curve = NULL;
+    unsigned char *private_key_bin = NULL;
+    secp256k1_pubkey *public_key = NULL;
     EVP_PKEY *keys = NULL;
     int elliptic_curve_nid;
 
@@ -23,6 +31,9 @@ int main() {
     OpenSSL_add_all_algorithms();
     ERR_load_BIO_strings();
     ERR_load_crypto_strings();
+
+    /* Inicializacao da biblioteca secp256k1 */
+    ctx = secp256k1_context_create(SECP256K1_FLAGS_TYPE_CONTEXT);
 
     /* IO */
     // Alocacao para prints do IO
@@ -41,32 +52,37 @@ int main() {
     // Geracao das chaves a partir da curva eliptica
     if (!(EC_KEY_generate_key(elliptic_curve)))
         BIO_printf(io_print, "Error generating public and private keys.");
-    // Estrutura utilizada para armazenas as chaves
-    keys = EVP_PKEY_new();
-    // Armazena as chaves geradas na estrutura 
-    if (!EVP_PKEY_assign_EC_KEY(keys, elliptic_curve))
-        BIO_printf(io_print, "Error when saving public and private keys.");
+    // Armazenamento da chave privada
+    const BIGNUM *private_key = EC_KEY_get0_private_key(elliptic_curve);
+    BN_print(io_print, private_key);
+    // Chave privada em binario (32 bytes)
+    private_key_bin = malloc(BN_num_bytes(private_key));
+    if (!(BN_bn2bin(private_key, private_key_bin)))
+        BIO_printf(io_print, "Error transforming private key to binary.");
+    printf("\n%s", private_key_bin);
+    // Calculo da chave publica a partir da chave privada
+    if (!(secp256k1_ec_pubkey_create(ctx, public_key, private_key_bin)))
+        BIO_printf(io_print, "Error generating the public key.");
+    printf("\n%s", public_key->data);
 
     /* Infos da curva eliptica */
-    // Obtem a curva eliptica a partir da estrutura
+    // Estrutura PEM utilizada para armazenas as chaves
+    keys = EVP_PKEY_new();
+    // Armazena as chaves geradas na estrutura PEM
+    if (!EVP_PKEY_assign_EC_KEY(keys, elliptic_curve))
+        BIO_printf(io_print, "Error when saving public and private keys.");
+    // Obtem a curva eliptica a partir da estrutura PEM
     elliptic_curve = EVP_PKEY_get1_EC_KEY(keys);
     // Obtem o grupo da curva eliptica
     const EC_GROUP *elliptic_curve_group = EC_KEY_get0_group(elliptic_curve);
     // Imprime na tela o tamanho das chaves
-    BIO_printf(io_print, "Keys size: %d bits\n", EVP_PKEY_bits(keys));
+    BIO_printf(io_print, "\nKey size: %d bits\n", EVP_PKEY_bits(keys));
     // Tipo da curva eliptica utilizada
-    BIO_printf(io_print, "Elliptic Curve type: %s\n", OBJ_nid2ln(EC_GROUP_get_curve_name(elliptic_curve_group)));
-
-    /* Impressao das chaves */
-    // Impressao completa da chave privada
-    if(!PEM_write_bio_PKCS8PrivateKey(io_print, keys, NULL, NULL, 0, 0, NULL))
-        BIO_printf(io_print, "Error printing private key in PEM format");
-    // Impressao da chave publica no formato SubjectPublicKeyInfo
-    if(!PEM_write_bio_PUBKEY(io_print, keys))
-        BIO_printf(io_print, "Error printing public key in PEM format");
+    BIO_printf(io_print, "Elliptic Curve: %s\n", OBJ_nid2ln(EC_GROUP_get_curve_name(elliptic_curve_group)));
 
     // Liberando os ponteiros alocados
     EVP_PKEY_free(keys);
+    free(private_key_bin);
     EC_KEY_free(elliptic_curve);
     BIO_free_all(io_print);
 
