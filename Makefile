0a4d88586f9bc63df6369f734613fb0367169b2e
Lucas Santos <lucasnascimentoss@gmail.com>
2019-03-29 19:51:10 -0300

    DONE: Generate Public Key (secp256k1) from a OpenSSL Private Key TODO: SHA256 and Naive Schnorr

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 compile_generate_keys:
-	gcc -o generate_keys generate_keys.c -lssl -lcrypto
+	gcc -o generate_keys -I ../secp256k1/src/ -I ../secp256k1/ generate_keys.c -lssl -lcrypto -lgmp
 
 generate_keys:
 	./generate_keys
--- generate_keys.c
+++ generate_keys.c
@@ -12,20 +12,23 @@
 
 /* secp256k1 */
 // Biblioteca de curva eliptica
+#include "../secp256k1/src/libsecp256k1-config.h"
 #include "../secp256k1/include/secp256k1.h"
+#include "../secp256k1/src/secp256k1.c"
 
 // Curva eliptica utilizada na criptografia de chave publica do Bitcoin
 #define DEFAULT_ELLIPTIC_CURVE "secp256k1"
 
 int main() {
              
-    secp256k1_context *ctx = NULL;
-    BIO *io_print = NULL;
-    EC_KEY *elliptic_curve = NULL;
-    unsigned char *private_key_bin = NULL;
-    secp256k1_pubkey *public_key = NULL;
-    EVP_PKEY *keys = NULL;
-    int elliptic_curve_nid;
+    secp256k1_context *ctx = NULL;          // Contexto
+    secp256k1_callback *cb = NULL;          // Callback para tratamento de erros
+    BIO *io_print = NULL;                   // Print OpenSSL
+    EC_KEY *elliptic_curve = NULL;          // Curva eliptica OpenSSL
+    unsigned char *private_key_bin = NULL;  // Chave privada em binario (32 bytes)
+    secp256k1_pubkey *public_key;           // Chave publica em binario (64 bytes)
+    EVP_PKEY *keys = NULL;                  // Armazenamento das chaves OpenSSL
+    int elliptic_curve_nid;                 // Nome da curva eliptica OpenSSL
 
     /* Inicializacao da biblioteca OpenSSL */
     OpenSSL_add_all_algorithms();
@@ -33,7 +36,10 @@ int main() {
     ERR_load_crypto_strings();
 
     /* Inicializacao da biblioteca secp256k1 */
-    ctx = secp256k1_context_create(SECP256K1_FLAGS_TYPE_CONTEXT);
+    ctx = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
+    // Inicializacao das operacoes de multiplicacao
+    secp256k1_ecmult_gen_context_init(&ctx->ecmult_gen_ctx);
+    secp256k1_ecmult_gen_context_build(&ctx->ecmult_gen_ctx, cb);
 
     /* IO */
     // Alocacao para prints do IO
@@ -54,23 +60,27 @@ int main() {
         BIO_printf(io_print, "Error generating public and private keys.");
     // Armazenamento da chave privada
     const BIGNUM *private_key = EC_KEY_get0_private_key(elliptic_curve);
+    printf("Chave privada em BIGNUM: ");
     BN_print(io_print, private_key);
     // Chave privada em binario (32 bytes)
     private_key_bin = malloc(BN_num_bytes(private_key));
     if (!(BN_bn2bin(private_key, private_key_bin)))
         BIO_printf(io_print, "Error transforming private key to binary.");
-    printf("\n%s", private_key_bin);
+    printf("\nChave privada em binario (unsigned char)[%d]: %s\n", BN_num_bytes(private_key), private_key_bin);
+    // Verificacao da validade da chave privada
+    if (!(secp256k1_ec_seckey_verify(ctx, private_key_bin)))
+        BIO_printf(io_print, "Error validating the private key.");
     // Calculo da chave publica a partir da chave privada
     if (!(secp256k1_ec_pubkey_create(ctx, public_key, private_key_bin)))
         BIO_printf(io_print, "Error generating the public key.");
-    printf("\n%s", public_key->data);
+    printf("\nChave publica em binario (unsigned char)[%lu]: %s\n", sizeof(public_key->data), public_key->data);
 
     /* Infos da curva eliptica */
     // Estrutura PEM utilizada para armazenas as chaves
     keys = EVP_PKEY_new();
     // Armazena as chaves geradas na estrutura PEM
     if (!EVP_PKEY_assign_EC_KEY(keys, elliptic_curve))
-        BIO_printf(io_print, "Error when saving public and private keys.");
+        BIO_printf(io_print, "Error when saving public and private keys (PEM Format).");
     // Obtem a curva eliptica a partir da estrutura PEM
     elliptic_curve = EVP_PKEY_get1_EC_KEY(keys);
     // Obtem o grupo da curva eliptica
