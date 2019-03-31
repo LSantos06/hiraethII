9a68af1832561e79d5b4f85dabb290fdfe384af8
Lucas Santos <lucasnascimentoss@gmail.com>
2019-03-31 00:03:00 -0300

    Still in the same error scalar_set_b32

--- main.c
+++ main.c
@@ -21,19 +21,15 @@
 int main() {
             
     /**** generate_keys ****/
-    BIO *io_print = NULL;                   // Print OpenSSL
-    EC_KEY *elliptic_curve = NULL;          // Curva eliptica OpenSSL
+    BIO *io_print = NULL;                                                // Print OpenSSL
+    EC_KEY *elliptic_curve = NULL;                                       // Curva eliptica OpenSSL
 
-    int elliptic_curve_nid;                 // Nome da curva eliptica OpenSSL
-    unsigned char *private_key = NULL;      // Chave privada em binario (32 bytes)
+    int elliptic_curve_nid;                                              // Nome da curva eliptica OpenSSL
+    unsigned char *private_key = calloc(1, sizeof(unsigned char));       // Chave privada em binario (32 bytes)
 
-    secp256k1_context *ctx = NULL;          // Contexto
-    secp256k1_callback *cb = NULL;          // Callback para tratamento de erros
-    secp256k1_pubkey public_key;            // Chave publica em binario (64 bytes)
-
-    /* Inicializacao da biblioteca OpenSSL */
-    ERR_load_BIO_strings();
-    OpenSSL_add_all_algorithms();
+    secp256k1_context *ctx = calloc(1, sizeof(secp256k1_context));       // Contexto
+    secp256k1_callback *cb = calloc(1, sizeof(secp256k1_callback));      // Callback para tratamento de erros
+    secp256k1_pubkey *public_key = calloc(1, sizeof(secp256k1_pubkey));  // Chave publica em binario (64 bytes)
 
     /* Inicializacao da biblioteca secp256k1 */
     ctx = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
@@ -72,24 +68,24 @@ int main() {
     if (!(secp256k1_ec_seckey_verify(ctx, private_key)))
         BIO_printf(io_print, "Error validating the private key.");
     // Calculo da chave publica a partir da chave privada
-    if (!(secp256k1_ec_pubkey_create(ctx, &public_key, private_key)))
+    if (!(secp256k1_ec_pubkey_create(ctx, public_key, private_key)))
         BIO_printf(io_print, "Error generating the public key.");
-    printf("\nChave publica em binario (unsigned char)[%lu]: %s\n", sizeof((&public_key)->data), (&public_key)->data);
+    printf("\nChave publica em binario (unsigned char)[%lu]: %s\n", sizeof(public_key->data), public_key->data);
 
-    /**** sign schnorr ****/
-    unsigned char message[32] = "Charles Leclerc";
-    unsigned char private_key_message[64];
-    unsigned char hashed[32];
-    unsigned char R_binary[32];
+    /**** sign_schnorr ****/
+    unsigned char *message = calloc(4, sizeof(unsigned char));
+    unsigned char *private_key_message = calloc(8, sizeof(unsigned char));
+    unsigned char *hashed = calloc(4, sizeof(unsigned char));
+    unsigned char *R_binary = calloc(4, sizeof(unsigned char));
 
-    secp256k1_sha256 hash;
+    secp256k1_sha256 *hash = calloc(1, sizeof(secp256k1_sha256));
 
-    secp256k1_scalar hashed_scalar;
-    secp256k1_scalar zero_scalar;
-    secp256k1_scalar nonce;
-    secp256k1_scalar R;
+    secp256k1_scalar *hashed_scalar = calloc(1, sizeof(secp256k1_scalar));
+    secp256k1_scalar *zero_scalar = calloc(1, sizeof(secp256k1_scalar));
+    secp256k1_scalar *nonce = calloc(1, sizeof(secp256k1_scalar));
+    secp256k1_scalar *R = calloc(1, sizeof(secp256k1_scalar));
 
-    //secp256k1_gej group_element_jacobian;
+    secp256k1_gej *group_element_jacobian = calloc(1, sizeof(secp256k1_gej));
 
     /* Assinatura Schnorr - R */
     // Let nonce = (scalar(0) + scalar(hash(bytes(private_key) || message))) mod group_order
@@ -97,43 +93,35 @@ int main() {
     // Let R = nonce G.
 
     // private_key_message = bytes(private_key) || message
+    message = "Charles Leclerc";
     strcat(private_key_message, private_key);
     strcat(private_key_message, message);
-    printf("\nprivate_key_message (unsigned char)[%lu]: %s\n", sizeof(private_key_message), private_key_message);
+    printf("\nprivate_key_message (unsigned char)[%lu]: %s\n", 8 * sizeof(private_key_message), private_key_message);
     // hashed = hash(bytes(private_key) || message)
-    secp256k1_sha256_initialize(&hash);
-    secp256k1_sha256_write(&hash, private_key_message, sizeof(private_key_message));
-    secp256k1_sha256_finalize(&hash, hashed);
-    printf("\nhashed (unsigned char)[%lu]: %s\n", sizeof(hashed), hashed);
+    secp256k1_sha256_initialize(hash);
+    secp256k1_sha256_write(hash, private_key_message, 8 * sizeof(private_key_message));
+    secp256k1_sha256_finalize(hash, hashed);
+    printf("\nhashed (unsigned char)[%lu]: %s\n", 4 * sizeof(hashed), hashed);
 
     // hashed_scalar = scalar(hash(bytes(private_key) || message))
-    secp256k1_scalar_set_b32(&hashed_scalar, hashed, NULL);
-    if ((secp256k1_scalar_is_zero(&hashed_scalar)))
-        BIO_printf(io_print, "hashed_scalar is 0.");
-    printf("\nhashed_scalar: %" PRIu32 "\n", hashed_scalar);
+    secp256k1_scalar_set_b32(hashed_scalar, hashed, NULL);
+    printf("\nhashed_scalar: %" PRIu32 "\n", *hashed_scalar);
     // scalar(0)
-    secp256k1_scalar_clear(&zero_scalar);
-    if (!(secp256k1_scalar_is_zero(&zero_scalar)))
-        BIO_printf(io_print, "zero_scalar isnt 0.");
+    secp256k1_scalar_clear(zero_scalar);
+    printf("\nzero_scalar: %" PRIu32 "\n", *zero_scalar);
     // nonce = (scalar(0) + scalar(hash(bytes(private_key) || message))) mod group_order
-    secp256k1_scalar_add(&nonce, &hashed_scalar, &zero_scalar);
-    if(secp256k1_scalar_is_zero(&nonce))
-        BIO_printf(io_print, "nonce is 0");
-    printf("\nnonce: %" PRIu32 "\n", nonce);
+    secp256k1_scalar_add(nonce, hashed_scalar, zero_scalar);
+    printf("\nnonce: %" PRIu32 "\n", *nonce);
     
     // R = nonce G
-    //R = nonce;
-    //secp256k1_ecmult_gen(&ctx->ecmult_gen_ctx, &group_element_jacobian, &R);
-    //if(secp256k1_scalar_is_zero(&R))
-    //    BIO_printf(io_print, "R equals 0");
-    //secp256k1_scalar_get_b32(R_binary, &R);
-    //printf("\nR (unsigned char)[%lu]: %s\n", sizeof(R_binary), R_binary);  
+    R = nonce;
+    secp256k1_ecmult_gen(&ctx->ecmult_gen_ctx, group_element_jacobian, R);
+    if(secp256k1_scalar_is_zero(R))
+        BIO_printf(io_print, "R equals 0");
+    secp256k1_scalar_get_b32(R_binary, R);
+    printf("\nR (unsigned char)[%lu]: %s\n", 4 * sizeof(R_binary), R_binary);
 
     // Liberando os ponteiros alocados
-    EVP_cleanup();
-    free(private_key);
-    EC_KEY_free(elliptic_curve);
-    BIO_free_all(io_print);
 
     exit(0);
 }
