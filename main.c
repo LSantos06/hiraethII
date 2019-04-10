857575e438f963816a132927e52448e4b4ed5386
Lucas Santos <lucasnascimentoss@gmail.com>
2019-04-10 16:46:52 -0300

    DONE: Verify ERROR: Coordinates of verification's R point

--- main.c
+++ main.c
@@ -39,6 +39,8 @@ int main() {
     // Inicializacao das operacoes de multiplicacao
     secp256k1_ecmult_gen_context_init(&ctx->ecmult_gen_ctx);
     secp256k1_ecmult_gen_context_build(&ctx->ecmult_gen_ctx, cb);
+    secp256k1_ecmult_context_init(&ctx->ecmult_ctx);
+    secp256k1_ecmult_context_build(&ctx->ecmult_ctx, cb);
 
     /* IO */
     // Alocacao para prints do IO
@@ -88,7 +90,6 @@ int main() {
     unsigned char *yR = calloc(32, sizeof(unsigned char));                       // Coordenada afim Y do ponto R em 32 bytes
     unsigned char *xR = calloc(32, sizeof(unsigned char));                       // Coordenada afim X do ponto R em 32 bytes
     unsigned char *xR_public_key_message = calloc(128, sizeof(unsigned char));   // Coordenada afim X do ponto R concatenada com a chave publica e com a mensagem em 128 bytes
-    unsigned char *hashed2 = calloc(32, sizeof(unsigned char));                  // Mensagem hasheada de 32 bytes
 
     unsigned char *k_bin = calloc(32, sizeof(unsigned char));                    // k de 32 bytes
 
@@ -119,9 +120,11 @@ int main() {
     secp256k1_num *yR_num = calloc(1, sizeof(secp256k1_num));                    // Coordenada afim Y do ponto R em num
     secp256k1_num *k_num = calloc(1, sizeof(secp256k1_num));                     // k em num
 
+    int overflow = 0;
+
     /* Assinatura Schnorr - R */
     // Let nonce = int(hash(bytes(private_key) || message)) mod group_order
-    //      Fail if nonce = 0
+    //      Fail if nonce = 0.
     // Let R = nonce G.
     //
     // private_key_message = bytes(private_key) || message
@@ -135,7 +138,9 @@ int main() {
     secp256k1_sha256_finalize(hash, hashed);
     printf("\nhashed (unsigned char)[%lu]: %s\n", 4 * sizeof(hashed), hashed);
     // nonce = int(hash(bytes(private_key) || message)) mod group_order
-    secp256k1_scalar_set_b32(nonce, hashed, NULL);
+    secp256k1_scalar_set_b32(nonce, hashed, &overflow);
+    if (overflow)
+        BIO_printf(io_print, "ERROR overflow nonce.");
     printf("\nnonce (secp256k1_scalar): %" PRIu64 "\n", nonce->d[0]);
     printf("nonce (secp256k1_scalar): %" PRIu64 "\n", nonce->d[1]);
     printf("nonce (secp256k1_scalar): %" PRIu64 "\n", nonce->d[2]);
@@ -144,7 +149,9 @@ int main() {
     if (secp256k1_scalar_is_zero(nonce))
         BIO_printf(io_print, "ERROR nonce = 0.");
     // R = nonce G
-    secp256k1_scalar_set_b32(R, hashed, NULL);
+    secp256k1_scalar_set_b32(R, hashed, &overflow);
+    if (overflow)
+        BIO_printf(io_print, "ERROR overflow R.");
     secp256k1_ecmult_gen(&ctx->ecmult_gen_ctx, &R_jacobian, R);
     // Coordenadas jacobianas do ponto R para coordenadas afim do ponto R
     secp256k1_ge_set_gej(&R_affine, &R_jacobian);
@@ -159,10 +166,10 @@ int main() {
 
     /* Assinatura Schnorr - s */
     // if jacobi(y(R)) = 1
-    //      Let k = nonce
+    //      Let k = nonce.
     // otherwise 
-    //      Let k = group_order - nonce
-    // Let e = int(hash(bytes(x(R)) || bytes(public_key) || message)) mod group_order
+    //      Let k = group_order - nonce.
+    // Let e = int(hash(bytes(x(R)) || bytes(public_key) || message)) mod group_order.
     // Let s = bytes(k + e private_key mod group_order).
     //
     // yR = bytes(y(R))
@@ -172,13 +179,17 @@ int main() {
     // group_order
     secp256k1_scalar_order_get_num(group_order);
     secp256k1_num_get_bin(group_order_bin, 4 * sizeof(group_order_bin), group_order);
-    secp256k1_scalar_set_b32(group_order_scalar, group_order_bin, NULL);
+    secp256k1_scalar_set_b32(group_order_scalar, group_order_bin, &overflow);
+    if (overflow)
+        BIO_printf(io_print, "ERROR overflow group_order_scalar.");
     // if jacobi(y(R)) = 1
     if (secp256k1_num_jacobi(yR_num, group_order) == 1)
     {
         printf("\njacobi(y(R)) = 1\n");
         // k = int(nonce) mod group_order
-        secp256k1_scalar_set_b32(k, hashed, NULL);
+        secp256k1_scalar_set_b32(k, hashed, &overflow);
+        if (overflow)
+            BIO_printf(io_print, "ERROR overflow k.");
     }
     // otherwise
     else
@@ -200,16 +211,20 @@ int main() {
     memcpy(xR_public_key_message, xR, 4 * sizeof(xR));
     memcpy(xR_public_key_message + (4 * sizeof(xR)), public_key->data, sizeof(public_key->data));
     memcpy(xR_public_key_message + (4 * sizeof(xR)) + sizeof(public_key->data), message, 4 * sizeof(message));
-    printf("\nxR_public_key_message (unsigned char)[%lu]: %s\n", (4 * sizeof(xR)) + + sizeof(public_key->data) + (4 * sizeof(message)), xR_public_key_message);
-    // hashed2 = hash(bytes(x(R)) || bytes(public_key) || message)
+    printf("\nxR_public_key_message (unsigned char)[%lu]: %s\n", (4 * sizeof(xR)) + sizeof(public_key->data) + (4 * sizeof(message)), xR_public_key_message);
+    // hashed = hash(bytes(x(R)) || bytes(public_key) || message)
     secp256k1_sha256_initialize(hash);
     secp256k1_sha256_write(hash, xR_public_key_message, 16 * sizeof(xR_public_key_message));
-    secp256k1_sha256_finalize(hash, hashed2);
-    printf("\nhashed2 (unsigned char)[%lu]: %s\n", 4 * sizeof(hashed2), hashed2);
+    secp256k1_sha256_finalize(hash, hashed);
+    printf("\nhashed2 (unsigned char)[%lu]: %s\n", 4 * sizeof(hashed), hashed);
     // e = int(hash(bytes(x(R)) || bytes(public_key) || message)) mod group_order
-    secp256k1_scalar_set_b32(e, hashed2, NULL);
+    secp256k1_scalar_set_b32(e, hashed, &overflow);
+    if (overflow)
+        BIO_printf(io_print, "ERROR overflow e.");
     // private_key_scalar = int(private_key) mod group_order
     secp256k1_scalar_set_b32(private_key_scalar, private_key, NULL);
+    if (overflow)
+        BIO_printf(io_print, "ERROR overflow private_key_scalar.");
     // mul = e private_key mod group_order
     secp256k1_scalar_mul(mul, e, private_key_scalar);
     printf("\nmul (secp256k1_scalar): %" PRIu64 "\n", mul->d[0]);
@@ -238,35 +253,103 @@ int main() {
     /**** verify_schnorr ****/
 
     unsigned char *compressed_public_key = calloc(33, sizeof(unsigned char));
-    const unsigned char field_size[] = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F";
+    const unsigned char field_size[32] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                                          0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFE, 0xFF, 0xFF, 
+                                          0xFC, 0x2F};
+    const unsigned char group_order_teste[32] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 
+                                                 0xFE, 0xBA, 0xAE, 0xDC, 0xE6, 0xAF, 0x48, 0xA0, 0x3B, 0xBF, 0xD2, 0x5E, 0x8C, 0xD0, 0x36, 
+                                                 0x41, 0x41};
+
+    unsigned char *r = calloc(32, sizeof(unsigned char));                       
+
+    unsigned char *r_P_message = calloc(97, sizeof(unsigned char));
 
     size_t compressed_public_key_size = 33;
 
-    secp256k1_ge P;
+    secp256k1_ge P_affine;
+    secp256k1_gej P_jacobian;
 
     int compare;
 
-    // Let P = point(public_key); fail if point(public_key) fails.
-    // Let r = int(sign[0:32]); fail if r ≥ field_size.
-    // Let s = int(sign[32:64]); fail if s ≥ group_order.
+    // Let P = point(public_key) 
+    //      Fail if point(public_key) fails.
+    // Let r = int(sign[0:32])
+    //      Fail if r ≥ field_size.
+    // Let s = int(sign[32:64])
+    //      Fail if s ≥ group_order.
     // Let e = int(hash(bytes(r) || bytes(P) || message)) mod group_order.
     // Let R = s G - e P.
-    // Fail if infinite(R) or jacobi(y(R)) ≠ 1 or x(R) ≠ r.
+    //      Fail if infinite(R) 
+    //      Fail if jacobi(y(R)) ≠ 1
+    //      Fail if x(R) ≠ r.
     //
+    // Verificacao da memoria group_order
+    compare = memcmp(group_order_teste, group_order_bin, 32);
+    if (compare != 0)
+        BIO_printf(io_print, "ERROR group_order.");
+    // Obtem a parte r da assinatura
+    memcpy(r, xR, 4 * sizeof(xR));
     // Comprime a chave publica para o tamanho de 33 bytes
     secp256k1_ec_pubkey_serialize(ctx, compressed_public_key, &compressed_public_key_size, public_key, SECP256K1_EC_COMPRESSED);
     printf("\ncompressed_public_key (unsigned char)[%d]: %s\n", 33, compressed_public_key);
-    // P = point(public_key)
-    if (!(secp256k1_eckey_pubkey_parse(&P, compressed_public_key, compressed_public_key_size)))
+    // P = point(public_key) 
+    //      Fail if point(public_key) fails.
+    if (!(secp256k1_eckey_pubkey_parse(&P_affine, compressed_public_key, compressed_public_key_size)))
         BIO_printf(io_print, "ERROR generating public key point.");
-    // r = int(sign[0:32]); fail if r ≥ field_size
-    compare = memcmp(xR, field_size, 32);
-    if(compare > 0)
+    // r = int(sign[0:32])
+    //      Fail if r ≥ field_size.
+    compare = memcmp(r, field_size, 32);
+    if (compare > 0 || compare == 0)
         BIO_printf(io_print, "ERROR r ≥ field_size.");
-    // s = int(sign[32:64]); fail if s ≥ group_order
-    compare = memcmp(s, group_order_bin, 32);
-    if(compare > 0)
+    // s = int(sign[32:64])
+    //      Fail if s ≥ group_order.
+    compare = memcmp(s, group_order_teste, 32);
+    if (compare > 0 || compare == 0)
         BIO_printf(io_print, "ERROR s ≥ group_order.");
+    // r_P_message = bytes(r) || bytes(P) || message
+    memcpy(r_P_message, r, 4 * sizeof(r));
+    memcpy(r_P_message + (4 * sizeof(r)), compressed_public_key, 33);
+    memcpy(r_P_message + (4 * sizeof(r)) + 33, message, (4 * sizeof(message)));
+    printf("\nr_P_message (unsigned char)[%lu]: %s\n", (4 * sizeof(r)) + 33 + (4 * sizeof(message)), r_P_message);
+    // hashed = hash(bytes(r) || bytes(P) || message)
+    secp256k1_sha256_initialize(hash);
+    secp256k1_sha256_write(hash, r_P_message, 97);
+    secp256k1_sha256_finalize(hash, hashed);
+    printf("\nhashed3 (unsigned char)[%lu]: %s\n", 4 * sizeof(hashed), hashed);
+    // e = int(hash(bytes(r) || bytes(P) || message)) mod group_order
+    secp256k1_scalar_set_b32(e, hashed, &overflow);
+    if (overflow)
+        BIO_printf(io_print, "ERROR overflow e.");
+    // e = -e
+    secp256k1_scalar_negate(e, e);
+    // Coordenadas afim do ponto P para coordenadas jacobianas do ponto P
+    secp256k1_gej_set_ge(&P_jacobian, &P_affine);
+    // R = - e P + s G
+    //      Fail if infinite(R) 
+    //      Fail if jacobi(y(R)) ≠ 1
+    //      Fail if x(R) ≠ r.
+    secp256k1_ecmult(&ctx->ecmult_ctx, &R_jacobian, &P_jacobian, e, sum);
+    // Coordenadas jacobianas do ponto R para coordenadas afim do ponto R
+    secp256k1_ge_set_gej(&R_affine, &R_jacobian);
+    // Normalizacao das coordenadas afim do ponto R
+    secp256k1_fe_normalize(&R_affine.y);
+    secp256k1_fe_normalize(&R_affine.x);
+    //      Fail if infinite(R) 
+    if (secp256k1_gej_is_infinity(&R_jacobian) || secp256k1_ge_is_infinity(&R_affine))
+        BIO_printf(io_print, "ERROR R is infinity.");
+    //      Fail if jacobi(y(R)) ≠ 1
+    // yR = bytes(y(R))
+    secp256k1_fe_get_b32(yR, &R_affine.y);
+    // yR_num = num(yR)
+    secp256k1_num_set_bin(yR_num, yR, 4 * sizeof(yR));
+    if (secp256k1_num_jacobi(yR_num, group_order) != 1)
+        BIO_printf(io_print, "ERROR jacobi(y(R)) ≠ 1.");
+    //      Fail if x(R) ≠ r.
+    // xR = bytes(x(R))
+    secp256k1_fe_get_b32(xR, &R_affine.x);
+    compare = memcmp(xR, r, 32);
+    if (compare != 0)
+        BIO_printf(io_print, "ERROR x(R) ≠ r.");
 
     // Liberando os ponteiros alocados
 
